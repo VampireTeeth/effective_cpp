c6f2fbdcc8f75b1e4bf362dae32ab93961068bdf
VampireTeeth <vampire001@gmail.com>
2013-11-12 10:26:44 +1100

    item24 added

---
+++ item24/src/item24.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : item24.cpp
+// Author      : Steven Liu
+// Version     :
+// Copyright   : All rights reserved
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	return 0;
+}
